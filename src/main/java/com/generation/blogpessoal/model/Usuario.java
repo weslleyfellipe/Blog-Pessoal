520ddadd034757e66546ec741253a62747baae7a
Weslley <weslleyfellipe1@gmail.com>
2024-06-07 11:43:41 -0300

    Deploy do Projeto Blog Pessoal

--- src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -28,8 +29,9 @@ public class Usuario {
 	 @NotNull(message ="Atributo Nome é Obrigatorio !")
 	 private String nome;
 	 
-	 @NotNull(message ="Atributo Email é Obrigatorio !")
-	 @Email(message = "Atributo deve ser um e-mail Valido")
+	 @Schema(example = "email@email.com.br")
+	 @NotNull(message = "O Atributo Usuário é Obrigatório!")
+	 @Email(message = "O Atributo Usuário deve ser um email válido!")
 	 private String usuario;
 	 
 	 @NotBlank(message = "O atributo Senha é obrigatorio")
@@ -39,10 +41,21 @@ public class Usuario {
     @Size(max = 5000, message = "A foto deve ter no maximo 5000 caracteres") 
 	 private String foto;
 
+   
 
     @OneToMany(fetch = FetchType.LAZY,mappedBy="usuario", cascade=CascadeType.REMOVE)
     @JsonIgnoreProperties("usuario")
     private List<Postagem> postagem;
+    
+	public Usuario(Long id, String nome, String usuario, String senha, String foto) {
+		this.id = id;
+		this.nome = nome;
+		this.usuario = usuario;
+		this.senha = senha;
+		this.foto = foto;
+	}
+	
+	public Usuario() { }
 
 
 	public Long getId() {
